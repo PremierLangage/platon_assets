8e264d2368372c94653f9e67835ab9a5dab167d5
JUILLARD <tjuillar@etud.u-pem.fr>
2021-02-19 12:33:56 +0100

    fix: bug with admin

--- apps/pl_resources/admin.py
+++ apps/pl_resources/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Circle, Resource, File
+
+admin.site.register(Circle)
+admin.site.register(Resource)
+admin.site.register(File)
